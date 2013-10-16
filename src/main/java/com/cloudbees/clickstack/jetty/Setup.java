465202969a7246dda0a2a3cd613055a42ae3590a
Cyrille Le Clerc <cyrille@cyrilleleclerc.com>
2013-10-17 00:19:08 +0200

    Major refactoring: use gradle-clickstack-plugin and clickstack-framework

---
+++ src/main/java/com/cloudbees/clickstack/jetty/Setup.java
@@ -0,0 +1,311 @@
+/*
+ * Copyright 2010-2013, CloudBees Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.cloudbees.clickstack.jetty;
+
+import com.cloudbees.clickstack.domain.environment.Environment;
+import com.cloudbees.clickstack.domain.metadata.Database;
+import com.cloudbees.clickstack.domain.metadata.Email;
+import com.cloudbees.clickstack.domain.metadata.Metadata;
+import com.cloudbees.clickstack.domain.metadata.SessionStore;
+import com.cloudbees.clickstack.util.ApplicationUtils;
+import com.cloudbees.clickstack.util.CommandLineUtils;
+import com.cloudbees.clickstack.util.Files2;
+import com.cloudbees.clickstack.util.Manifests;
+import com.google.common.base.Charsets;
+import com.google.common.base.Preconditions;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Collections2;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collection;
+import java.util.Collections;
+
+public class Setup {
+
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+    @Nonnull
+    final Path appDir;
+    @Nonnull
+    final Path genappDir;
+    @Nonnull
+    final Path controlDir;
+    @Nonnull
+    final Path clickstackDir;
+    @Nonnull
+    final Path warFile;
+    @Nonnull
+    final Path logDir;
+    @Nonnull
+    final Path tmpDir;
+    @Nonnull
+    final Path agentLibDir;
+    @Nonnull
+    final Path appExtraFilesDir;
+    @Nonnull
+    final Metadata metadata;
+    @Nonnull
+    final Environment env;
+    /**
+     * initialised by {@link #installJettyHome()}
+     */
+    @Nullable
+    Path jettyHome;
+
+
+    public Setup(@Nonnull Environment env, @Nonnull Metadata metadata) throws IOException {
+        logger.info("Setup: {}, {}", env, metadata);
+
+        this.env = env;
+        this.appDir = env.appDir;
+
+        this.genappDir = env.genappDir;
+
+        this.controlDir = env.controlDir;
+        this.logDir = Files.createDirectories(genappDir.resolve("log"));
+        Files2.chmodAddReadWrite(logDir);
+
+        this.agentLibDir = Files.createDirectories(appDir.resolve("javaagent-lib"));
+
+        this.tmpDir = Files.createDirectories(appDir.resolve("tmp"));
+        Files2.chmodAddReadWrite(tmpDir);
+
+        this.clickstackDir = env.clickstackDir;
+        Preconditions.checkState(Files.exists(clickstackDir) && Files.isDirectory(clickstackDir));
+
+        this.warFile = env.packageDir.resolve("app.war");
+        Preconditions.checkState(Files.exists(warFile), "File not found %s", warFile);
+        Preconditions.checkState(!Files.isDirectory(warFile), "Expected to be a file and not a directory %s", warFile);
+
+        this.appExtraFilesDir = Files.createDirectories(appDir.resolve("app-extra-files"));
+        Files2.chmodAddReadWrite(appExtraFilesDir);
+
+        this.metadata = metadata;
+
+        logger.debug("warFile: {}", warFile.toAbsolutePath());
+        logger.debug("agentLibDir: {}", agentLibDir.toAbsolutePath());
+        logger.debug("appExtraFilesDir: {}", appExtraFilesDir.toAbsolutePath());
+    }
+
+    public static void main(String[] args) throws Exception {
+        try {
+            Logger initialisationLogger = LoggerFactory.getLogger(Setup.class);
+
+            initialisationLogger.info("Setup clickstack {} - {}, current dir {}",
+                    Manifests.getAttribute(Setup.class, "Implementation-Artifact"),
+                    Manifests.getAttribute(Setup.class, "Implementation-Date"),
+                    FileSystems.getDefault().getPath(".").toAbsolutePath());
+            Environment env = CommandLineUtils.argumentsToEnvironment(args);
+            Path metadataPath = env.genappDir.resolve("metadata.json");
+            Metadata metadata = Metadata.Builder.fromFile(metadataPath);
+
+            Setup setup = new Setup(env, metadata);
+            setup.setup();
+        } catch (Exception e) {
+            String hostname;
+            try {
+                hostname = InetAddress.getLocalHost().getHostAddress();
+            } catch (Exception e2) {
+                hostname = "#hostname#";
+            }
+            throw new Exception("Exception deploying on " + hostname, e);
+        }
+    }
+
+    public void setup() throws Exception {
+        installJettyHome();
+        installSkeleton();
+        installApplication();
+        installCloudBeesJavaAgent();
+        installJmxTransAgent();
+        writeJavaOpts();
+        writeConfig();
+        installControlScripts();
+        installJettyJavaOpts();
+
+        SetupJettyConfigurationFiles setupTomcatConfigurationFiles = new SetupJettyConfigurationFiles(metadata);
+        setupTomcatConfigurationFiles.buildJettyConfiguration(jettyHome);
+        logger.info("Clickstack successfully installed");
+    }
+
+    public void installSkeleton() throws IOException {
+        logger.debug("installSkeleton() {}", appDir);
+
+        Files2.copyDirectoryContent(clickstackDir.resolve("dist"), appDir);
+    }
+
+    public void installJettyJavaOpts() throws IOException {
+        Path optsFile = controlDir.resolve("java-opts-20-jetty-opts");
+        logger.debug("installJettyJavaOpts() {}", optsFile);
+        String opts = "" +
+                "-Djava.io.tmpdir=\"" + tmpDir + "\" " +
+                "-Djetty.home=\"" + jettyHome + "\" " +
+                "-Dapp_extra_files=\"" + appExtraFilesDir + "\" " +
+                "-Djetty.port=" + env.appPort + "";
+
+        Files.write(optsFile, Collections.singleton(opts), Charsets.UTF_8);
+    }
+
+    public void installJettyHome() throws Exception {
+
+        Path jettyPackagePath = Files2.findArtifact(clickstackDir, "jetty-distribution", "zip");
+        Files2.unzip(jettyPackagePath, appDir);
+        jettyHome = Files2.findUniqueFolderBeginningWith(appDir, "jetty-distribution");
+        logger.debug("installJettyHome() {}", jettyHome);
+
+        Files2.copyDirectoryContent(clickstackDir.resolve("resources/jetty-home"), jettyHome);
+
+
+        Path targetLibDir = Files.createDirectories(jettyHome.resolve("lib/ext"));
+        Files2.copyDirectoryContent(clickstackDir.resolve("deps/jetty-lib"),targetLibDir );
+
+        // JDBC Drivers
+        Collection<Database> mysqlDatabases = Collections2.filter(metadata.getResources(Database.class), new Predicate<Database>() {
+            @Override
+            public boolean apply(@Nullable Database database) {
+                return Database.DRIVER_MYSQL.equals(database.getDriver());
+            }
+        });
+        if (!mysqlDatabases.isEmpty()) {
+            logger.debug("Add mysql jars");
+            Files2.copyDirectoryContent(clickstackDir.resolve("deps/jetty-lib-mysql"), targetLibDir);
+        }
+
+        Collection<Database> postgresqlDatabases = Collections2.filter(metadata.getResources(Database.class), new Predicate<Database>() {
+            @Override
+            public boolean apply(@Nullable Database database) {
+                return Database.DRIVER_POSTGRES.equals(database.getDriver());
+            }
+        });
+        if (!postgresqlDatabases.isEmpty()) {
+            Files2.copyDirectoryContent(clickstackDir.resolve("deps/jetty-lib-postgresql"), targetLibDir);
+        }
+
+        // Mail
+        if (!metadata.getResources(Email.class).isEmpty()) {
+            logger.debug("Add mail jars");
+            Files2.copyDirectoryContent(clickstackDir.resolve("deps/jetty-lib-mail"), targetLibDir);
+        }
+
+        // Memcache
+        if (!metadata.getResources(SessionStore.class).isEmpty()) {
+            logger.debug("Add memcache jars");
+            Files2.copyDirectoryContent(clickstackDir.resolve("deps/jetty-lib-memcache"), targetLibDir);
+        }
+
+        Files2.chmodReadOnly(jettyHome);
+    }
+
+    public Path installApplication() throws IOException {
+        logger.debug("installCatalinaBase() {}", jettyHome);
+
+        Path rootWebAppDir = Files.createDirectories(jettyHome.resolve("webapps"));
+        Files2.copyToDirectory(warFile, rootWebAppDir);
+
+        ApplicationUtils.extractApplicationExtraFiles(warFile, appDir);
+        ApplicationUtils.extractContainerExtraLibs(warFile, Files.createDirectories(jettyHome.resolve("lib/ext")));
+
+        Files2.chmodAddReadWrite(jettyHome);
+
+        return jettyHome;
+    }
+
+    public void installJmxTransAgent() throws IOException {
+        logger.debug("installJmxTransAgent() {}", agentLibDir);
+
+        Path jmxtransAgentJarFile = Files2.copyArtifactToDirectory(clickstackDir.resolve("deps/javaagent-lib"), "jmxtrans-agent", agentLibDir);
+        Path jmxtransAgentConfigurationFile = jettyHome.resolve("jetty-metrics.xml");
+        Preconditions.checkState(Files.exists(jmxtransAgentConfigurationFile), "File %s does not exist", jmxtransAgentConfigurationFile);
+        Path jmxtransAgentDataFile = logDir.resolve("jetty-metrics.data");
+
+        Path agentOptsFile = controlDir.resolve("java-opts-60-jmxtrans-agent");
+
+        String agentOptsFileData =
+                "-javaagent:" + jmxtransAgentJarFile.toString() + "=" + jmxtransAgentConfigurationFile.toString() +
+                        " -Djetty_metrics_data_file=" + jmxtransAgentDataFile.toString();
+
+        Files.write(agentOptsFile, Collections.singleton(agentOptsFileData), Charsets.UTF_8);
+    }
+
+    public void installCloudBeesJavaAgent() throws IOException {
+        logger.debug("installCloudBeesJavaAgent() {}", agentLibDir);
+
+        Path cloudbeesJavaAgentJarFile = Files2.copyArtifactToDirectory(clickstackDir.resolve("deps/javaagent-lib"), "cloudbees-clickstack-javaagent", this.agentLibDir);
+        Path agentOptsFile = controlDir.resolve("java-opts-20-javaagent");
+
+        Path envFile = controlDir.resolve("env");
+        if (!Files.exists(envFile)) {
+            logger.error("Env file not found at {}", envFile);
+        }
+        String agentOptsFileData = "-javaagent:" +
+                cloudbeesJavaAgentJarFile +
+                "=sys_prop:" + envFile;
+
+        Files.write(agentOptsFile, Collections.singleton(agentOptsFileData), Charsets.UTF_8);
+    }
+
+    public void writeJavaOpts() throws IOException {
+        Path javaOptsFile = controlDir.resolve("java-opts-10-core");
+        logger.debug("writeJavaOpts() {}", javaOptsFile);
+
+        String javaOpts = metadata.getRuntimeParameter("java", "opts", "");
+        Files.write(javaOptsFile, Collections.singleton(javaOpts), Charsets.UTF_8);
+    }
+
+    public void writeConfig() throws IOException {
+
+        Path configFile = controlDir.resolve("config");
+        logger.debug("writeConfig() {}", configFile);
+
+        PrintWriter writer = new PrintWriter(Files.newOutputStream(configFile));
+
+        writer.println("app_dir=\"" + appDir + "\"");
+        writer.println("app_tmp=\"" + appDir.resolve("tmp") + "\"");
+        writer.println("log_dir=\"" + logDir + "\"");
+        writer.println("jetty_home=\"" + jettyHome + "\"");
+
+        writer.println("port=" + env.appPort);
+
+        Path javaPath = env.getJavaExecutable(metadata);
+        writer.println("java=\"" + javaPath + "\"");
+        Path javaHome = env.getJavaHome(metadata);
+        writer.println("JAVA_HOME=\"" + javaHome + "\"");
+        writer.println("genapp_dir=\"" + genappDir + "\"");
+
+        writer.close();
+    }
+
+    public void installControlScripts() throws IOException {
+        logger.debug("installControlScripts() {}", controlDir);
+
+        Files2.chmodAddReadExecute(controlDir);
+
+        Path genappLibDir = genappDir.resolve("lib");
+        Files.createDirectories(genappLibDir);
+
+        Path jmxInvokerPath = Files2.copyArtifactToDirectory(clickstackDir.resolve("deps/control-lib"), "cloudbees-jmx-invoker", genappLibDir);
+        // create symlink without version to simplify jmx_invoker script
+        Files.createSymbolicLink(genappLibDir.resolve("cloudbees-jmx-invoker-jar-with-dependencies.jar"), jmxInvokerPath);
+    }
+}
---
+++ src/main/java/com/cloudbees/clickstack/jetty/SetupJettyConfigurationFiles.java
@@ -0,0 +1,182 @@
+/*
+* Copyright 2010-2013, CloudBees Inc.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package com.cloudbees.clickstack.jetty;
+
+import com.cloudbees.clickstack.domain.metadata.*;
+import com.cloudbees.clickstack.util.XmlUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.annotation.Nonnull;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.logging.Logger;
+
+public class SetupJettyConfigurationFiles {
+
+    private final Logger logger = Logger.getLogger(getClass().getName());
+    private Metadata metadata;
+    private Set<String> databaseProperties = new HashSet(Arrays.asList("minIdle", "maxIdle", "maxActive", "maxWait",
+            "initialSize",
+            "validationQuery", "validationQueryTimeout", "testOnBorrow", "testOnReturn",
+            "timeBetweenEvictionRunsMillis", "numTestsPerEvictionRun", "minEvictableIdleTimeMillis", "testWhileIdle",
+            "removeAbandoned", "removeAbandonedTimeout", "logAbandoned", "defaultAutoCommit", "defaultReadOnly",
+            "defaultTransactionIsolation", "poolPreparedStatements", "maxOpenPreparedStatements", "defaultCatalog",
+            "connectionInitSqls", "connectionProperties", "accessToUnderlyingConnectionAllowed",
+            "type", "validatorClassName", "initSQL", "jdbcInterceptors", "validationInterval", "jmxEnabled",
+            "fairQueue", "abandonWhenPercentageFull", "maxAge", "useEquals", "suspectTimeout", "rollbackOnReturn",
+            "commitOnReturn", "alternateUsernameAllowed", "useDisposableConnectionFacade", "logValidationErrors",
+            "propagateInterruptState"));
+
+    public SetupJettyConfigurationFiles(@Nonnull Metadata metadata) {
+        this.metadata = metadata;
+    }
+
+    protected void addXForwardedForSupport(Document jettyXmlDocument) {
+        Element httpConfig = XmlUtils.getUniqueElement(jettyXmlDocument, "/Configure/New[@id='httpConfig']");
+
+        Element callElement = jettyXmlDocument.createElement("Call");
+        callElement.setAttribute("name", "addCustomizer");
+        httpConfig.appendChild(callElement);
+
+        Element argElement = jettyXmlDocument.createElement("Arg");
+        callElement.appendChild(argElement);
+
+        Element newElement = jettyXmlDocument.createElement("New");
+        newElement.setAttribute("class", "org.eclipse.jetty.server.ForwardedRequestCustomizer");
+        argElement.appendChild(newElement);
+    }
+
+    /**
+     * See <a href="http://wiki.eclipse.org/Jetty/Howto/Configure_JNDI_Datasource">Jetty/Howto/Configure JNDI Datasource</a>
+     */
+    protected SetupJettyConfigurationFiles addDatabase(Database database, Document contextDocument) {
+        //Add database Jetty 9
+        logger.info("Insert DataSource " + database.getName());
+
+        // Jetty <jdbc-connection-pool>
+        Element dataSource = contextDocument.createElement("New");
+        dataSource.setAttribute("id", database.getName());
+        dataSource.setAttribute("class", "org.eclipse.jetty.plus.jndi.Resource");
+
+        Element jndiName = contextDocument.createElement("Arg");
+        jndiName.setTextContent("jdbc/" + database.getName());
+        dataSource.appendChild(jndiName);
+
+        Element objectToBind = contextDocument.createElement("Arg");
+        dataSource.appendChild(objectToBind);
+
+
+        Element dataSourceInstance = contextDocument.createElement("New");
+        dataSourceInstance.setAttribute("class", "org.apache.tomcat.jdbc.pool.DataSource");
+        objectToBind.appendChild(dataSourceInstance);
+
+        Map<String, String> params = new TreeMap<>();
+        params.put("driverClassName", database.getJavaDriver());
+        params.put("url", "jdbc:" + database.getUrl());
+        params.put("username", database.getUsername());
+        params.put("password", database.getPassword());
+
+        // by default max to 20 connections which is the limit of CloudBees MySQL databases
+        params.put("maxActive", "20");
+        params.put("maxIdle", "10");
+        params.put("minIdle", "1");
+
+        // test on borrow and while idle to release idle connections
+        params.put("testOnBorrow", "true");
+        params.put("testWhileIdle", "true");
+        params.put("validationQuery", database.getValidationQuery());
+        params.put("validationInterval", "5000"); // 5 secs
+
+        for (Map.Entry<String, String> param : params.entrySet()) {
+            dataSourceInstance.appendChild(createJettyConfigSetDirective(param.getKey(), param.getValue(), contextDocument));
+        }
+
+        contextDocument.getDocumentElement().appendChild(dataSource);
+        return this;
+    }
+
+    private Element createJettyConfigSetDirective(String name, String value, Document contextDocument) {
+        Element setElement = contextDocument.createElement("Set");
+        setElement.setAttribute("name", name);
+        setElement.setTextContent(value);
+        return setElement;
+    }
+
+    protected SetupJettyConfigurationFiles addEmail(Email email, Document appXmlDocument) {
+        logger.warning("Ignore addEmail(" + email + ")");
+        return this;
+    }
+
+    protected SetupJettyConfigurationFiles addSessionStore(SessionStore store, Document appXmlDocument) {
+        logger.warning("Ignore addSessionStore(" + store + ")");
+
+        return this;
+    }
+
+    protected SetupJettyConfigurationFiles addPrivateAppValve(Metadata metadata, Document appXmlDocument) {
+        logger.warning("Ignore addPrivateAppValve(" + metadata + ")");
+
+        return this;
+    }
+
+    protected void buildJettyConfiguration(Metadata metadata, Document jettyXmlDocument, Document appXmlDocument) throws ParserConfigurationException {
+
+        String message = "File generated by jetty9-clickstack at " + new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").format(new Date());
+
+        appXmlDocument.appendChild(appXmlDocument.createComment(message));
+        jettyXmlDocument.appendChild(jettyXmlDocument.createComment(message));
+
+        for (Resource resource : metadata.getResources().values()) {
+            if (resource instanceof Database) {
+                addDatabase((Database) resource, appXmlDocument);
+            } else if (resource instanceof Email) {
+                addEmail((Email) resource, appXmlDocument);
+            } else if (resource instanceof SessionStore) {
+                addSessionStore((SessionStore) resource, appXmlDocument);
+            }
+        }
+        addPrivateAppValve(metadata, appXmlDocument);
+
+        addXForwardedForSupport(jettyXmlDocument);
+    }
+
+    public void buildJettyConfiguration(@Nonnull Path jettyHome) throws Exception {
+
+        Path jettyXmlFile = jettyHome.resolve("etc/jetty.xml");
+        Document jettyXmlDocument = XmlUtils.loadXmlDocumentFromPath(jettyXmlFile);
+        XmlUtils.checkRootElement(jettyXmlDocument, "Configure");
+
+        Path appXmlFile = jettyHome.resolve("webapps/app.xml");
+
+        Document appXmlDocument = XmlUtils.loadXmlDocumentFromPath(appXmlFile);
+        XmlUtils.checkRootElement(appXmlDocument, "Configure");
+
+        this.buildJettyConfiguration(metadata, jettyXmlDocument, appXmlDocument);
+
+        // see http://permalink.gmane.org/gmane.comp.ide.eclipse.jetty.user/1745
+        Map<String, String> outputProperties = new HashMap<>();
+        outputProperties.put(OutputKeys.DOCTYPE_PUBLIC, "-//Mort Bay Consulting//DTD Configure//EN");
+        outputProperties.put(OutputKeys.DOCTYPE_SYSTEM, "http://jetty.mortbay.org/configure.dtd");
+
+        XmlUtils.flush(appXmlDocument, Files.newOutputStream(appXmlFile), outputProperties);
+        XmlUtils.flush(jettyXmlDocument, Files.newOutputStream(jettyXmlFile), outputProperties);
+    }
+}
